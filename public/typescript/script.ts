82b9a4028501c02335b7bcc3fc24a9cb0778e6f4
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-02 16:57:51 -0300

    correct import

--- public/typescript/script.ts
+++ public/typescript/script.ts
@@ -1,5 +1,6 @@
 import ModalProtocol from "./modal/ModalProtocol.js";
 import StyleDeposit from "./inputs/StyleDeposit.js";
+import LoadProtocols from "./load/LoadProtocols.js";
 
 const modalProtocol = new ModalProtocol(
     '[data-protocol="buttonNew"]',
@@ -12,3 +13,16 @@ modalProtocol.init();
 
 const styleDeposit = new StyleDeposit('[data-protocol="deposit"]');
 styleDeposit.init();
+
+const loadProtocols = new LoadProtocols(
+    "http://localhost:3000",
+    '[data-load="icon"]',
+    '[data-load="alert"]',
+    '[data-load="public"]',
+    '[data-load="doc"]',
+    '[data-load="title"]',
+    '[data-load="depositDefeated"]',
+    '[data-load="defeated"]',
+    '[data-load="completed"]'
+);
+loadProtocols.init();
