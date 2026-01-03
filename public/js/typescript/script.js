034ec0d801e5b1604ce61cb9d6cd3f26470a8fb5
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-03 17:08:51 -0300

    init dayjs packege to use in project

--- public/js/typescript/script.js
+++ public/js/typescript/script.js
@@ -1,6 +1,8 @@
 import ModalProtocol from "./modal/ModalProtocol.js";
 import StyleDeposit from "./inputs/StyleDeposit.js";
 import LoadProtocols from "./load/LoadProtocols.js";
+import dayjs from "./dayjs.js";
+console.log(dayjs().format("DD/MM/YYYY"));
 const modalProtocol = new ModalProtocol('[data-protocol="buttonNew"]', '[data-protocol="modal"]', '[data-protocol="dateEnvio"]', '[data-protocol="buttonClose"]', "hidden");
 modalProtocol.init();
 const styleDeposit = new StyleDeposit('[data-protocol="deposit"]');
--- public/typescript/script.ts
+++ public/typescript/script.ts
@@ -1,6 +1,9 @@
 import ModalProtocol from "./modal/ModalProtocol.js";
 import StyleDeposit from "./inputs/StyleDeposit.js";
 import LoadProtocols from "./load/LoadProtocols.js";
+import dayjs from "./dayjs.js";
+
+console.log(dayjs().format("DD/MM/YYYY"));
 
 const modalProtocol = new ModalProtocol(
     '[data-protocol="buttonNew"]',
