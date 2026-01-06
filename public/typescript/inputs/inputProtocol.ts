7f5189963019ccd684d5f44db0fca57d9e315092
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-06 10:10:39 -0300

    remove console.log

--- public/typescript/inputs/inputProtocol.ts
+++ public/typescript/inputs/inputProtocol.ts
@@ -5,9 +5,5 @@ export default function inputProtocol(dataInput: string) {
 
     input.maxLength = 6;
 
-    input.addEventListener('input', () => {
-        input.value = input.value.replace(/[^0-9]/g, '');
-        console.log(input.value);
-        
-    });
+    input.addEventListener('input', () => input.value = input.value.replace(/[^0-9]/g, ''));
 }
