df14ae72804fe2c0b5ec7b897b23569791a6c59c
Leonardo2831 <leoferraz1818@gmail.com>
2026-01-05 21:19:36 -0300

    added dayjs in modalProtocol

--- public/typescript/modal/ModalProtocol.ts
+++ public/typescript/modal/ModalProtocol.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export default class ModalProtocol {
     buttonNew: HTMLElement | null;
     buttonClose: HTMLElement | null;
@@ -33,10 +35,10 @@ export default class ModalProtocol {
     newValueInput(): void {
         if (!this.inputDataInvit) return;
         
-        const date = new Date();
-        const day = String(date.getDate()).padStart(2, "0");
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const year = String(date.getFullYear());
+        const date = dayjs();
+        const day = String(date.date()).padStart(2, "0");
+        const month = String(date.month() + 1).padStart(2, "0");
+        const year = String(date.year());
 
         this.inputDataInvit.value = `${year}-${month}-${day}`;
     }
